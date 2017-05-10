f5378ea6a5021a7ab9b959cc16661da13c2e7ccc
Alessa Messineo <apmessineo@gmail.com>
2017-05-10 18:01:31 -0400

    add tab event listeners

--- app.js
+++ app.js
@@ -3,30 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('script loaded');
 
   //                    variables
-  // let dropDownSched = document.querySelector('.circle-sched i');
   let dropDownSched = document.querySelectorAll('.circle-sched i');
-  // let detailsP = document.querySelector('.details-p');
-  // let eventDiv = document.querySelector('.event');
   let day1 = document.getElementById('day1');
   let day2 = document.getElementById('day2');
   let day3 = document.getElementById('day3');
+  let day1events = document.querySelectorAll('.day1');
+  let day2events = document.querySelectorAll('.day2');
+  let day3events = document.querySelectorAll('.day3');
 
-  console.log('dropDownSched: ', dropDownSched);
+  //                   click events
 
+// does the drop down for event details
   for (let i = 0; i< dropDownSched.length; i++){
     let clickSched = 0;
     let eventDiv = dropDownSched[i].parentNode.parentNode.parentNode.parentNode
-    let detailsP = eventDiv.childNodes[3].childNodes[5]
+    let detailsP = eventDiv.childNodes[3].childNodes[5];
     dropDownSched[i].addEventListener('click', () => {
       if (clickSched%2 === 0){
         dropDownSched[i].classList.remove('down-arrow');
         dropDownSched[i].classList.add('up-arrow');
         clickSched++;
-        console.log('eventDiv.childNodes[3].childNodes[5]: ', eventDiv.childNodes[3].childNodes[5]);
 
         detailsP.classList.remove('inactive');
         detailsP.classList.add('active');
-
 
         eventDiv.setAttribute("style", "height:200px;background-color:#00ACE9;color:#FFFFFF;border:none");
 
@@ -46,36 +45,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  //                   click events
-
-  // let clickSched = 0;
-  // dropDownSched.addEventListener('click', () => {
-
-  //   if (clickSched%2 === 0){
-  //     dropDownSched.classList.remove('down-arrow');
-  //     dropDownSched.classList.add('up-arrow');
-  //     clickSched++;
-  //     detailsP.classList.remove('inactive');
-  //     detailsP.classList.add('active');
-
-  //     eventDiv.setAttribute("style", "height:200px;background-color:#00ACE9;color:#FFFFFF;border:none");
-
-  //   } else {
-  //     dropDownSched.classList.remove('up-arrow');
-  //     dropDownSched.classList.add('down-arrow');
-  //     clickSched++;
-  //     detailsP.classList.remove('active');
-  //     detailsP.classList.add('inactive');
-
-  //     eventDiv.setAttribute("style", "height:120px;background-color:#FFFFFF; color:#5C5C5C;border:2px solid #25D432");
-
-  //   }
-  // }) // closes dropDownSched
 
 
 
-
-
+// switches between days
   day1.addEventListener('click', ()=> {
     day1.classList.add('tab-active');
     day2.classList.remove('tab-active');
